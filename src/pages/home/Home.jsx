ab80b2cbbc8f4d3bf3e2129c9afce957a466ce5b
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-10-28 12:41:04 +0500

    Added components, routes, navbar and MUI packages

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./home.scss"
+import Sidebar from '../../components/sidebar/Sidebar'
+
+export default function Home() {
+  return (
+    <div className='home'>
+      <Sidebar />
+      <div className="homeContainer">
+        container
+      </div>
+    </div>
+  )
+}
