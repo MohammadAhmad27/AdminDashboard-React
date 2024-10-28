8618810ec5543232c1c0837f598eea13a15dd5e9
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-10-28 20:46:47 +0500

    Added Widget component to Home.jsx, and modified home.scss to include styles for widgets: Update title in index.html

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./home.scss"
 import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar'
+import Widget from '../../components/widget/Widget'
 
 export default function Home() {
   return (
@@ -9,7 +10,15 @@ export default function Home() {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        home container
+        <div className="widgets">
+          <Widget type='user' />
+          <Widget type='order' />
+          <Widget type='earning' />
+          <Widget type='balance' />
+        </div>
+        <div className="charts">
+          
+        </div>
       </div>
     </div>
   )
