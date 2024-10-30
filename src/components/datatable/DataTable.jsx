f67407c9fa0074433f797355593dbdddf588f55a
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-10-30 22:49:40 +0500

    Updated App.js, DataTable.jsx, datatable.scss, and Sidebar.jsx with new imports, styles, and link components

--- src/components/datatable/DataTable.jsx
+++ src/components/datatable/DataTable.jsx
@@ -1,38 +1,47 @@
 import "./datatable.scss"
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import Paper from '@mui/material/Paper';
 import { userColumns, userRows } from "../../datatablesource";
+import { Link } from "react-router-dom";
 
 
 const paginationModel = { page: 0, pageSize: 9 };
 
 export default function DataTable() {
-    const actionColumn = [
-        {
-          field: "action",
-          headerName: "Action",
-          width: 200,
-          renderCell: (params) => {
-            return (
-              <div className="cellAction">
-                  <div className="viewButton">View</div>
-                <div className="deleteButton">Delete</div>
-              </div>
-            );
-          },
-        },
-      ];
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="cellAction">
+            <Link to='/users/test' style={{ textDecoration: 'none' }}>
+              <div className="viewButton">View</div>
+            </Link>
+            <div className="deleteButton">Delete</div>
+          </div>
+        );
+      },
+    },
+  ];
 
-    return (
-        <div className="datatable">
-            <DataGrid
-                rows={userRows}
-                columns={userColumns.concat(actionColumn)}
-                initialState={{ pagination: { paginationModel } }}
-                pageSizeOptions={[5, 10]}
-                checkboxSelection
-            />
-        </div>
-    );
+  return (
+    <div className="datatable">
+      <div className="datatableTitle">
+        Add New User
+        <Link to='/users/new' style={{ textDecoration: 'none' }} className="link">
+          Add New
+        </Link>
+      </div>
+      <DataGrid
+      className="datagrid"
+        rows={userRows}
+        columns={userColumns.concat(actionColumn)}
+        initialState={{ pagination: { paginationModel } }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
+  );
 }
