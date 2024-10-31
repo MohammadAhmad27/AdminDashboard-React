d72ec681bec34f4b1ba15f429203740489c5659d
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-11-01 00:14:40 +0500

    Updated DataTable component to use state for data, added delete functionality, and made styling changes to datatable and navbar components

--- src/components/datatable/DataTable.jsx
+++ src/components/datatable/DataTable.jsx
@@ -2,12 +2,20 @@ import "./datatable.scss"
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { userColumns, userRows } from "../../datatablesource";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 
 const paginationModel = { page: 0, pageSize: 9 };
 
 export default function DataTable() {
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    const newRows = data.filter((row) => row.id !== id);
+    setData(newRows);
+  }
+
   const actionColumn = [
     {
       field: "action",
@@ -19,7 +27,7 @@ export default function DataTable() {
             <Link to='/users/test' style={{ textDecoration: 'none' }}>
               <div className="viewButton">View</div>
             </Link>
-            <div className="deleteButton">Delete</div>
+            <div className="deleteButton" onClick={() => handleDelete(params.row.id)}>Delete</div>
           </div>
         );
       },
@@ -35,8 +43,8 @@ export default function DataTable() {
         </Link>
       </div>
       <DataGrid
-      className="datagrid"
-        rows={userRows}
+        className="datagrid"
+        rows={data}
         columns={userColumns.concat(actionColumn)}
         initialState={{ pagination: { paginationModel } }}
         pageSizeOptions={[5, 10]}
