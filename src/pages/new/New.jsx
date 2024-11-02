eee08a79b2700d8501b0b93b1ceab321aca77801
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-11-02 21:25:40 +0500

    Added Firestore and Storage imports to firebase.js, updated form inputs in formSource.js, and implemented file upload and user creation functionality in New.jsx

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -2,10 +2,86 @@ import "./new.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { collection, addDoc, serverTimestamp, doc, setDoc } from "firebase/firestore";
+import { auth, db, storage } from "../../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
 
 const New = ({ inputs, title }) => {
     const [file, setFile] = useState("");
+    const [data, setData] = useState({});
+    const [per, setPer] = useState(null);
+
+
+    useEffect(() => {
+        const uploadFile = () => {
+
+            const name = new Date().getTime() + file.name;
+            console.log(name);
+
+            const storageRef = ref(storage, file.name);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            // Register three observers:
+            // 1. 'state_changed' observer, called any time the state changes
+            // 2. Error observer, called on failure
+            // 3. Completion observer, called on successful completion
+            uploadTask.on('state_changed',
+                (snapshot) => {
+                    // Observe state change events such as progress, pause, and resume
+                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log('Upload is ' + progress + '% done');
+                    setPer(progress);
+                    switch (snapshot.state) {
+                        case 'paused':
+                            console.log('Upload is paused');
+                            break;
+                        case 'running':
+                            console.log('Upload is running');
+                            break;
+                        default:
+                            break;
+                    }
+                },
+                (error) => {
+                    // Handle unsuccessful uploads
+                    console.log(error);
+                },
+                () => {
+                    // Handle successful uploads on complete
+                    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        // console.log('File available at', downloadURL);
+                        setData((prev) => ({ ...prev, img: downloadURL }));
+                    });
+                }
+            );
+        }
+        file && uploadFile();
+    }, [file])
+
+
+    const handleAddDoc = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await createUserWithEmailAndPassword(auth, data.email, data.password);
+            const docRef = await setDoc(doc(db, "users", res.user.uid), {
+                ...data,
+                timeStamp: serverTimestamp()
+            });
+        } catch (error) {
+            console.error("Error adding document: ", error);
+        }
+
+    };
+
+
+    const handleInput = (e) => {
+        setData({ ...data, [e.target.id]: e.target.value });
+    };
 
     return (
         <div className="new">
@@ -27,7 +103,7 @@ const New = ({ inputs, title }) => {
                         />
                     </div>
                     <div className="right">
-                        <form>
+                        <form onSubmit={handleAddDoc}>
                             <div className="formInput">
                                 <label htmlFor="file">
                                     Image: <DriveFolderUploadOutlinedIcon className="icon" />
@@ -43,10 +119,15 @@ const New = ({ inputs, title }) => {
                             {inputs.map((input) => (
                                 <div className="formInput" key={input.id}>
                                     <label>{input.label}</label>
-                                    <input type={input.type} placeholder={input.placeholder} />
+                                    <input
+                                        id={input.id}
+                                        type={input.type}
+                                        placeholder={input.placeholder}
+                                        onChange={handleInput}
+                                    />
                                 </div>
                             ))}
-                            <button>Send</button>
+                            <button disabled={per !== null && per < 100} type="submit">Send</button>
                         </form>
                     </div>
                 </div>
