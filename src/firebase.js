ab90e69bfe7c4980e0229e797ef0e5b3e99c4356
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-11-02 12:27:38 +0500

    Refactored Featured component and updated Firebase configuration to use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { apiKey } from "./api_key";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: apiKey,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
